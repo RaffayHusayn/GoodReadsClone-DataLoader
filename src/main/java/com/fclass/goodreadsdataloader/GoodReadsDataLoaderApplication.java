ecaa1b32b9bba6a92114656c502e6bb49d3118ac
Raffay Syed <raffayhusayn@gmail.com>
2022-03-23 23:39:15 -0400

    Started the project to stream the data of books and store it to a cassandra NoSQL instance

---
+++ src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.fclass.goodreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodReadsDataLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodReadsDataLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fclass.goodreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodReadsDataLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
