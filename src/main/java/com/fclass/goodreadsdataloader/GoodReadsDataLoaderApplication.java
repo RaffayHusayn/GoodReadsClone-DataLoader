a34672ffb80783e4dba29cadbe802b303f38b54c
Raffay Syed <raffayhusayn@gmail.com>
2022-03-24 20:54:59 -0400

    Add application.properties to connect to the remote cassandra instance hosted in datastax.astra
    -- Added login details
    -- Added schema-actions (Make new database based on java entities or
    not)
    -- Other info

--- src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
+++ src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoodReadsDataLoaderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GoodReadsDataLoaderApplication.class, args);
+//        SpringApplication.run(GoodReadsDataLoaderApplication.class, args);
+        System.out.println("something");
     }
 
 }
