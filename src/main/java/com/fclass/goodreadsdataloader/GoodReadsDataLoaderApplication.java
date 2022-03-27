025e0ad1f3c4db3595f32b75c585868ae6c8af2c
Raffay Syed <raffayhusayn@gmail.com>
2022-03-27 02:57:50 -0400

    Add another way to add manually created Author to the remote Cassandra Instance using CommandLineRunner
    -- Way # 1 : using @PostConstructor
    -- Way # 2 : using CommandLineRunner

--- src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
+++ src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
@@ -3,6 +3,7 @@ package com.fclass.goodreadsdataloader;
 import com.fclass.goodreadsdataloader.author.Author;
 import com.fclass.goodreadsdataloader.author.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -21,6 +22,23 @@ public class GoodReadsDataLoaderApplication {
         SpringApplication.run(GoodReadsDataLoaderApplication.class, args);
 
     }
+
+    // Using Setter Injection to inject AuthorRepository Bean
+    @Autowired
+    public void setAuthorRepository(AuthorRepository authorRepository){
+        this.authorRepository = authorRepository;
+    }
+
+    /*
+    ================================================================================
+        Adding manually created Authors in 2 ways:
+        1. Using @PostConstructor
+        2. Using CommnandLineRunner
+
+        use whatever you like, I'll use the CommandLineRunner
+    ================================================================================
+     */
+    //========= Way # 1 : Using PostConstructor ======================
     @PostConstruct
     public void start(){
         Author author = new Author();
@@ -30,10 +48,15 @@ public class GoodReadsDataLoaderApplication {
         authorRepository.save(author);
     }
 
-    @Autowired
-    public void setAuthorRepository(AuthorRepository authorRepository){
-        this.authorRepository = authorRepository;
+    //========= Way # 2 : Using CommandLineRunner =====================
+    @Bean
+    CommandLineRunner runner(AuthorRepository authorRepository) {
+        return args -> {
+            authorRepository.save(new Author("2", "Ayesha", "Ayesha Personal Name"));
+            authorRepository.save(new Author("3", "Nabia", "Nabia Personal Name"));
+        };
     }
+
 
     /*
     Using the downloaded Secure-Connect-Goodreads bundle to connect this SpringBoot
