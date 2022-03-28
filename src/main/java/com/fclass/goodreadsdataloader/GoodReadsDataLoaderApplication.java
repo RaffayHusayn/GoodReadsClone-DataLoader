85b0798c5a292a102377dfb78f06061d9f88902c
Raffay Syed <raffayhusayn@gmail.com>
2022-03-28 01:54:32 -0400

    Add method to initAuthor() to take Author data from the datadump and upload it to the cassandra instance
    -- the path of the author datadump file is mentioned in the
    application.properties files
    -- Using @Value annotation to get value from the application.properties
    to the class using a field injection

--- src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
+++ src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
@@ -2,7 +2,10 @@ package com.fclass.goodreadsdataloader;
 
 import com.fclass.goodreadsdataloader.author.Author;
 import com.fclass.goodreadsdataloader.author.AuthorRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +14,20 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 //@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class GoodReadsDataLoaderApplication {
     private AuthorRepository authorRepository;
+    @Value("${datadump.location.authors}")
+    private String authorsDumpLocation;
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
 
     public static void main(String[] args) {
         SpringApplication.run(GoodReadsDataLoaderApplication.class, args);
@@ -41,21 +52,23 @@ public class GoodReadsDataLoaderApplication {
     //========= Way # 1 : Using PostConstructor ======================
     @PostConstruct
     public void start(){
-        Author author = new Author();
-        author.setId("1");
-        author.setName("raffay");
-        author.setPersonalName("personal raffay");
-        authorRepository.save(author);
+//        Author author = new Author();
+//        author.setId("1");
+//        author.setName("raffay");
+//        author.setPersonalName("personal raffay");
+//        authorRepository.save(author);
+        intiAuthors();
+        System.out.println("done");
     }
 
     //========= Way # 2 : Using CommandLineRunner =====================
-    @Bean
-    CommandLineRunner runner(AuthorRepository authorRepository) {
-        return args -> {
-            authorRepository.save(new Author("2", "Ayesha", "Ayesha Personal Name"));
-            authorRepository.save(new Author("3", "Nabia", "Nabia Personal Name"));
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner(AuthorRepository authorRepository) {
+//        return args -> {
+//            authorRepository.save(new Author("2", "Ayesha", "Ayesha Personal Name"));
+//            authorRepository.save(new Author("3", "Nabia", "Nabia Personal Name"));
+//        };
+//    }
 
 
     /*
@@ -67,4 +80,38 @@ public class GoodReadsDataLoaderApplication {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
+
+
+    private void intiAuthors(){
+        Path path = Paths.get(authorsDumpLocation);
+            try (Stream<String> lines = Files.lines(path)) {
+                System.out.println("We have the lines");
+                lines.forEach(line -> {
+                    // 1. Read and Parse each line
+                    String jsonString = line.substring(line.indexOf("{")); //start the substring from { to the end
+                    System.out.println("jsonString : "+ jsonString);
+
+                    try {
+                       JSONObject jsonObject = new JSONObject(jsonString);
+
+                        //2. Construct the Author Object
+                        Author author = new Author();
+                        author.setName(jsonObject.optString("name"));
+                        author.setPersonalName(jsonObject.optString("personal_name"));
+                        author.setId(jsonObject.optString("key").replace("/authors/",""));
+
+                        //3. Save the Author Object in the remote instance using Repository
+                        authorRepository.save(author);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+    }
+
+
 }
