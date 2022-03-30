1b415c3e272d9ad27877ec99d6a2ab40a4bdd8dd
Raffay Syed <raffayhusayn@gmail.com>
2022-03-30 05:07:40 -0400

    Add code to extract data from work datadump and upload it to the remote cassandra instance

--- src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
+++ src/main/java/com/fclass/goodreadsdataloader/GoodReadsDataLoaderApplication.java
@@ -1,9 +1,11 @@
 package com.fclass.goodreadsdataloader;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fclass.goodreadsdataloader.author.Author;
 import com.fclass.goodreadsdataloader.author.AuthorRepository;
 import com.fclass.goodreadsdataloader.works.Work;
 import com.fclass.goodreadsdataloader.works.WorkRepository;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -147,6 +157,7 @@ public class GoodReadsDataLoaderApplication {
 
     private void initWork(){
         Path path = Paths.get(worksDumpLocation);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
         try(Stream<String> lines = Files.lines(path)){
             lines.forEach(line->{
                 //1. Read and Parse each line
@@ -158,10 +169,47 @@ public class GoodReadsDataLoaderApplication {
 
                     //2. Construct Work Object
                     Work work = new Work();
+                    //2(a) setting Work ID
                     work.setId(jsonObject.optString("key").replace("/works/", ""));
+                    // 2(b) setting Work Title
                     work.setTitle(jsonObject.optString("title"));
-                    work.setAuthorId(jsonObject.getJSONArray("authors").getJSONObject(0).getJSONObject("author").optString("key").replace("/authors/",""));
-                    System.out.println("author Id : "+ work.getAuthorId());
+                    //2(c) setting Work Description
+                    JSONObject descriptionObj = jsonObject.optJSONObject("description");
+                    if(descriptionObj != null){
+                        work.setDescription(descriptionObj.optString("value"));
+                    }
+                    //2(d) Setting Author Ids List
+                    //2(e) Setting Author Names List
+                    JSONArray authorObjList = jsonObject.optJSONArray("authors");
+                    if(authorObjList != null){
+                        List<String> authorIds = new ArrayList<>();
+                        for(int i=0 ; i<authorObjList.length(); i++){
+                            JSONObject authorObj = authorObjList.getJSONObject(i).getJSONObject("author");
+                            authorIds.add(authorObj.optString("key").replace("/authors/",""));
+
+                        }
+                        work.setAuthorIds(authorIds);
+                        List<String> authorNames = authorIds.stream().map(id->authorRepository.findById(id))
+                                .map(authorOptional ->{
+                                    if(authorOptional.isEmpty()) return "unknown";
+                                    return authorOptional.get().getName();
+                                } ).collect(Collectors.toList());
+                        work.setAuthorNames(authorNames);
+                    }
+                    // 2(f) Setting Cover Ids list
+                    JSONArray coverObjList = jsonObject.optJSONArray("covers");
+                    if(coverObjList != null){
+                        List<String> coverIds = new ArrayList<>();
+                        for(int i=0; i<coverObjList.length(); i++){
+                            coverIds.add(coverObjList.optString(i));
+                        }
+                        work.setCoverIds(coverIds);
+                    }
+                    // 2(g) Setting Published Date
+                    JSONObject published = jsonObject.optJSONObject("created");
+                    if(published != null){
+                        work.setPublishedDate(LocalDate.parse(published.optString("value"), dateFormat));
+                    }
 
                     //3. Save Work Object in the remote instance using workRepository
                     workRepository.save(work);
