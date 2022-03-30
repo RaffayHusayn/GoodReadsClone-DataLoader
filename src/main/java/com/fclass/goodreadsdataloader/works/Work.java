cc3633efa86333c506a2f9061f63c4519163d9dc
Raffay Syed <raffayhusayn@gmail.com>
2022-03-30 05:07:11 -0400

    Updated properties in the Work class that we need in the cassandra database

--- src/main/java/com/fclass/goodreadsdataloader/works/Work.java
+++ src/main/java/com/fclass/goodreadsdataloader/works/Work.java
@@ -9,8 +9,22 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 import org.springframework.data.cassandra.core.mapping.CassandraType.Name;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Table(value = "work_by_id")
 public class Work {
+    /*
+    Attributes:
+        1. Work ID
+        2. Description
+        3. Work Title
+        4. Author IDs List
+        5. Author Names List
+        6. Covers List
+        7. Published Date
+
+     */
 
     @PrimaryKeyColumn(name = "work_id" , ordinal =0 , type= PrimaryKeyType.PARTITIONED)
     @Id
@@ -20,19 +34,38 @@ public class Work {
     @CassandraType(type = Name.TEXT)
     private String title;
 
-    @Column(value = "author_id")
+
+    @Column(value = "description")
     @CassandraType(type = Name.TEXT)
-    private String authorId;
+    private String description;
 
-    //No Arg constructor
-    public Work() {
-    }
+    @Column(value = "author_ids")
+    @CassandraType(type = Name.LIST, typeArguments = Name.TEXT)
+    private List<String> authorIds;
 
-    // All Arg Constructor
-    public Work(String id, String title, String authorId) {
+    @Column(value = "author_names")
+    @CassandraType(type = Name.LIST, typeArguments = Name.TEXT)
+    private List<String> authorNames;
+
+    @Column(value = "covers")
+    @CassandraType(type = Name.LIST, typeArguments = Name.TEXT)
+    private List<String> coverIds;
+
+    @Column(value = "published_date")
+    @CassandraType(type = Name.DATE)
+    private LocalDate publishedDate;
+
+    public Work(String id, String title, String description, List<String> authorIds, List<String> authorNames, List<String> coverIds, LocalDate publishedDate) {
         this.id = id;
         this.title = title;
-        this.authorId = authorId;
+        this.description = description;
+        this.authorIds = authorIds;
+        this.authorNames = authorNames;
+        this.coverIds = coverIds;
+        this.publishedDate = publishedDate;
+    }
+
+    public Work() {
     }
 
     public String getId() {
@@ -51,11 +84,43 @@ public class Work {
         this.title = title;
     }
 
-    public String getAuthorId() {
-        return authorId;
+    public String getDescription() {
+        return description;
     }
 
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getAuthorIds() {
+        return authorIds;
+    }
+
+    public void setAuthorIds(List<String> authorIds) {
+        this.authorIds = authorIds;
+    }
+
+    public List<String> getAuthorNames() {
+        return authorNames;
+    }
+
+    public void setAuthorNames(List<String> authorNames) {
+        this.authorNames = authorNames;
+    }
+
+    public List<String> getCoverIds() {
+        return coverIds;
+    }
+
+    public void setCoverIds(List<String> coverIds) {
+        this.coverIds = coverIds;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
     }
 }
